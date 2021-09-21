d0ba76702e0c061e2c7082006c4f07d7fd633373
mort607e <55091901+mort607e@users.noreply.github.com>
2021-09-21 09:49:03 +0200

    Add files via upload

---
+++ Dummy.java
@@ -0,0 +1,10 @@
+package uge01;
+
+public class Dummy {
+	public static void main(String[] args) {
+		System.out.println("//////////////////////\r\n"
+				+ "|| Victory is mine! ||\r\n"
+				+ "\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+		}
+
+}
